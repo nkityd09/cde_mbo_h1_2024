77c013037892c00a835d1edee617a9617fc4018d
nkityd09 <ankity@cloudera.com>
2023-06-26 15:13:06 -0700

    Updated Financial News Scraper

--- cde_code/finviz.py
+++ cde_code/finviz.py
@@ -3,6 +3,16 @@ import pandas as pd
 from bs4 import BeautifulSoup as soup
 from urllib.request import Request, urlopen
 import requests
+import re
+from pyspark.sql import SparkSession
+import os
+
+spark = SparkSession\
+        .builder\
+        .appName("Write_To_S3")\
+        .enableHiveSupport()\
+        .getOrCreate()
+s3_write_location = "s3a://ankity-cdp-aw-delete/data/" 
 
 # Set up scraper
 url = "https://finviz.com/news.ashx"
@@ -11,51 +21,75 @@ webpage = urlopen(req).read()
 html = soup(webpage, "html.parser")
 
 
-# print(type(html))
-# # Define function to scrape and process news
-# def scrape_news(html, idx):
-#     try:
-#         news = pd.read_html(str(html))[idx]
-#         #news.columns = ["0", "Time", "Headlines"]
-#         #news = news.drop(columns=["0"])
-#         #news = news.set_index("Time")
-#         return news
-#     except Exception as e:
-#         print(f"Error: {e}")
-#         return None
+news_items = []
+for row in html.find_all("tr", class_="nn"):
+    date = row.find("td", class_="nn-date").text.strip()
+    link = row.find("a", class_="nn-tab-link")
+    title = row.find("a", class_="nn-tab-link")
+    source_icon = row.find("td", class_=re.compile("news_source_icon")).find("use")
+    news_items.append({"date": date, "link": link, "title": title, "source_icon": source_icon})
+
+titles = []
+links = []
+articles = []
+for item in news_items:
+    try:
+        link_html = item['link']
+        link = link_html.get("href")
+        links.append(link)
+        title_html = item['title']
+        title = [tag.text for tag in title_html]
+        titles.append(title[0])
+    except (TypeError, AttributeError):
+        continue
+# print(titles)  
+# print(links)
 
 
-# # Scrape and show general news
-# news_df = scrape_news(html, 5)
-# if news_df is not None:
-#     print("\nGeneral News: ")
-#     print(news_df)
-
-# # Scrape and show blog news
-# blog_news_df = scrape_news(html, 6)
-# if blog_news_df is not None:
-#     print("\nBlog News: ")
-#     print(blog_news_df)
+subset_links = links[0:10]
 
 
+for link in subset_links:
+  req = Request(link, headers={"User-Agent": "Mozilla/5.0"})
+  webpage = urlopen(req).read()
+  html = soup(webpage, "html.parser")
+  paragraphs = html.find_all('p')
 
-entries = html.find_all("tr", class_="nn")
-#print(entries)
+  # Extract the text from each paragraph
+  paragraph_texts = [paragraph.get_text() for paragraph in paragraphs]
+  merged_article = ' '.join(paragraph_texts)
+  articles.append(merged_article)
+  
 
+  # Create a DataFrame from the titles and string_data lists
+data = list(zip(titles, articles))
+df = spark.createDataFrame(data, ["title", "data"])
 
+for row in df.collect():
+    title = row["title"]
+    data = row["data"]
+    file_name = title.replace(" ", "_") + ".txt"
+    s3_path = s3_write_location + file_name
+    with open(file_name, "w") as file:
+        file.write(data)
+    spark.sparkContext.parallelize([file_name]).coalesce(1).saveAsTextFile(s3_path)
 
-entry = html.find("tr", class_="nn")
-print(entry)
-article_body = html.find("td", class_="news_tooltip-tab")
-print("######")
-print(article_body)
+# # Function to write each file to S3
+# def write_to_s3(row):
+#     title = row["title"]
+#     data = row["data"]
+#     file_name = title + ".txt"
+#     s3_path = s3_write_location + file_name
+    
+#     # Write data to a local file
+#     with open(file_name, "w") as file:
+#         file.write(data)
+    
+#     # Write the local file to S3
+#     spark.read.text(file_name).write.text(s3_path)
+    
+#     # Clean up the local file
+#     os.remove(file_name)
 
-# response = requests.get(link)
-# article_soup = soup(response.text, "html.parser")
-# article_body = article_soup.find("td", class_="news_tooltip-tab")
-
-# if article_body:
-#     article_body_text = article_body.text.strip()
-#     print(article_body_text)
-# else:
-#     print("Unable to find article body.")
+# # Apply the write_to_s3 function to each row of the DataFrame
+# df.foreach(write_to_s3)
--- cde_code/read_from_s3_cde.py
+++ cde_code/read_from_s3_cde.py
@@ -9,15 +9,31 @@ spark = SparkSession\
 
 #S3 Object URI to Read
 s3_read_location = "s3a://ankity-cdp-aw-delete/cde-data/news_data/articles1.csv"
-
+# s3_bucket = "ankity-cdp-aw-delete"
+# s3_prefix = "data/"
 
 #S3 Write Location
-#s3_write_location = "s3a://ankity-cdp-aw-delete/output_data/" 
+s3_write_location = "s3a://ankity-cdp-aw-delete/data/" 
 
 
 # Read CSV file from S3
 df = spark.read.format("csv").option("header","true").load(s3_read_location)
 
-df.show()
+def write_row_to_file(dataframe):
+    df = dataframe.select("content")
+#   file_path = f"s3://{s3_bucket}/{s3_prefix}{name}.txt"
+#   s3_client.put_object(Body=name, Bucket=s3_bucket, Key=f"{s3_prefix}{name}.txt")
+    #df.write.text(f"{s3_write_location}")
+    return df
+
+
+df_subset = df.limit(20)
+final_df = write_row_to_file(df_subset)
+
+final_df.show()
+
+
+
+
 # Write transformed file to S3
-#df.write.orc(f"{s3_write_location}/housing_orc")
+#df.write.text(f"{s3_write_location}/housing_orc")
